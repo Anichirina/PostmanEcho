cbd4ae554a3f82991fc7c20fab53bdee0fa7f655
anichkova <anichkova2016@yamdex.ru>
2021-03-28 23:40:32 +0300

    change Test

--- src/test/java/PostmanEchoApiTestV3.java
+++ src/test/java/PostmanEchoApiTestV3.java
@@ -29,4 +29,43 @@ public class PostmanEchoApiTestV3 {
         ;
         ;
     }
+
+    @Test
+    void shouldReturnDemoAccountsNew1() {
+        // Given - When - Then
+        // Предусловия
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("9")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("9"))
+
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsNew() {
+        // Given - When - Then
+        // Предусловия
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("value")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("value"))
+
+
+        ;
+    }
 }
